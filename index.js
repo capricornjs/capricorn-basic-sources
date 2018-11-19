b41ea9e81e1c16cfa6a0bc8e25d5f1557f40fe42
yangxi <599321378@qq.com>
2018-11-19 11:43:04 +0800

    feat: 修改toast挂载位置

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import Events from 'mona-events'
 import toast from 'components/toast'
 import './style/index.less'
 
-window.Capricorn = { toast }
+window.Capricorn = {}
+window.toast = toast
 
 // 摩羯系统全局消息监听机制，负责模块间通信
 window.Capricorn.Events = Events
